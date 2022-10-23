3aad5efbbb2b6473d0e0da6d70a29f807f7c96d9
Persues <60343592+PersuesT@users.noreply.github.com>
2022-10-23 23:35:09 +0200

    Update main.ts

--- setup/main.ts
+++ setup/main.ts
@@ -2,7 +2,7 @@ import { defineAppSetup } from "@slidev/types";
 import liveSlidev from "live-slidev";
 import key from "./key.json";
 
-const ROOM_ID = "c5e83723-9fe8-4fca-96a1-a15b70d1c59b";
+const ROOM_ID = "25bd261e-4d9f-49e2-b9ad-7b1c21931ce5";
 
 export default defineAppSetup((ctx) => {
     const ws = new WebSocket("wss://rooms.deno.dev/" + ROOM_ID);
